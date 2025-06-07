c85bf993a55c0c3eda55ce1a5956afffa0ed0c17
agungmargi <agunx.margi@gmail.com>
2025-06-07 09:53:14 +0700

    initialize project

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello word")
+import express, {Application, Request, Response, NextFunction} from "express";
+
+const app: Application = express();
+const port: Number = 4000;
+
+
+app.use("/", (req: Request, res:Response, next:NextFunction) => {
+    res.status(200).send({status: 200})
+})
+
+app.listen(port, () => console.log(`Server is listening on port, ${port}`));
+
+
